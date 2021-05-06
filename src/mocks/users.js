83088d604a8971da24b1427e2ef6367f17cf252a
cauefurui <cauefurui@gmail.com>
2021-05-06 17:14:11 -0300

    first commit

---
+++ src/mocks/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Caue',
+  },
+  {
+    id: 2,
+    name: 'Banana',
+  },
+  {
+    id: 3,
+    name: 'Maça',
+  },
+  {
+    id: 4,
+    name: 'Jose',
+  },
+  {
+    id: 5,
+    name: 'ana',
+  },
+]
